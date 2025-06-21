807fb2e1a9b5bde0d140db9d957cc669d9b1a9bd
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-06-21 07:20:44 +0100

    mongo2

--- config/db.js
+++ config/db.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };

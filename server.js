5d2a8b184adc2fe4f84a2e52e862669b329e54fa
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-06-21 07:43:22 +0100

    mongo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 
 const paystackCallback = require("./routes/shop/paystack-callback");
 
-console.log("ENV Vars Loaded:", process.env);
+// console.log("ENV Vars Loaded:", process.env);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
